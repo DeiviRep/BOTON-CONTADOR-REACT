49c5468738e7b6179ec882d60f727e7c61637f99
DeiviRep <patzivargasdavid@gmail.com>
2023-02-19 17:01:27 -0400

    V1 BOTON-CLIC 19/02/23

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import logo from "./img/misuse-3.png";
+import Boton from "./componentes/boton";
+import "./estilos.css/boton.css";
+import "./estilos.css/contador.css";
+import Contador from "./componentes/contador";
+import { useState } from "react";
 
 function App() {
+
+  const [numClics, setNumClics] = useState(0);
+
+  const manejarClic = () => {
+    setNumClics(numClics + 1);
+  };
+  const reiniciar = () => {
+    setNumClics(0);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="logo-contenedor">
+        <img className="logo" src={logo} alt="logo de frecocamp" />
+      </div>
+      <div className="contenedor-contador">
+        <Contador numClics = {numClics} />
+        <Boton texto="Clic" esBotonDeClic={true} manejarClic={manejarClic} />
+        <Boton
+          texto="Reiniciar"
+          esBotonDeClic={false}
+          manejarClic={reiniciar}
+        />
+      </div>
     </div>
   );
 }
